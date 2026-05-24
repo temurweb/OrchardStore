def5bd7d95b084bb076b214b727f53391fcb15ff
Temur Rashidov <rashidov27@outlook.com>
2026-05-24 16:34:50 +0500

    hero section bug fixed

--- src/pages/Home/components/HeroSection.jsx
+++ src/pages/Home/components/HeroSection.jsx
@@ -10,14 +10,14 @@ export default function HeroSection() {
  const { t } = useTranslation()
 
   return (
-    <div className='px-5 lg:px-10 py-10 bg-gray-200'>
+    <div className='px-5 lg:px-10 pt-10 bg-gray-200'>
       <div className='bg-green-600 rounded-3xl flex flex-col items-center lg:flex-row  lg:justify-between pt-10 px-5 md:px-20 h-[512px] overflow-hidden'>
         <div className=''>
           <h2 className='text-[24px] md:text-[32px] lg:text-[40px] text-yellow-500 font-bold'>{t("Home.title_1")}</h2>
           <p className='text-[20px] md:text-[24px] lg:text-[32px] text-white font-bold'>{t("Home.title_2")} <br className='hidden lg:block'/> {t("Home.title_2.5")}</p>
           <Link className='mt-4 ' to='/category'><button className=' w-52 lg:w-64 text-[24px] p-1 bg-yellow-500 text-white font-bold mt-4 rounded-full shadow-md shadow-yellow-500 cursor-pointer'>{t("button_1")}</button></Link>
         </div>
-        <img className='w-[250px] lg:w-[410px]' src={man_withproducts} alt="man" />
+        <img className='w-[200px] lg:w-[410px]' src={man_withproducts} alt="man" />
       </div>
     </div>
   )
